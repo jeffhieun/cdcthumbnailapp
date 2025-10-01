fa7991fd38d13d78d923e1789bf969150e9a0a58
Hieu Nguyen <9hieu_nguyen@singaporeair.com.sg>
2025-10-01 10:28:20 +0700

    fix bugs when thumbnail folder delete. it will regenerated

--- src/main/java/com/cathay/cdc/thumbnail/poc/ThumbnailPocApplication.java
+++ src/main/java/com/cathay/cdc/thumbnail/poc/ThumbnailPocApplication.java
@@ -6,12 +6,20 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Collections;
+
 @SpringBootApplication(scanBasePackages = "com.cathay.cdc.thumbnail.poc")
 @EnableJpaRepositories("com.cathay.cdc.thumbnail.poc.repository")
 @EntityScan("com.cathay.cdc.thumbnail.poc.entity")
 @EnableScheduling
 public class ThumbnailPocApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ThumbnailPocApplication.class, args);
+        SpringApplication app = new SpringApplication(ThumbnailPocApplication.class);
+        // Override server.port from environment variable if present
+        String port = System.getenv("PORT");
+        if (port != null) {
+            app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        }
+        app.run(args);
 	}
 }
